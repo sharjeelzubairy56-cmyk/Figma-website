0fd58fe574333a1705d49fa4fb5cfdc82e2e9b24
sharjeelzubairy56-cmyk <sharjeelzubairy56@gmail.com>
2026-07-20 00:42:14 +0500

    fixed some other things and did a bit item details

--- figma-website/src/ItemDetails.jsx
+++ figma-website/src/ItemDetails.jsx
@@ -1,10 +1,15 @@
 import { Link } from 'react-router-dom'
 function ItemDetails() {
     return (
-        <>
-            <div>I am the items detail</div>
-            <button><Link to="/Cart">Hi I am the cart</Link></button>
-        </>
+        <div className="itemdetails">
+            <div className="idimage"><img src="shirt.jpg" height="100%" width="100%"/></div>
+            <div className="details">
+                <div className="tsdetail">T-Shirt with tape details</div>
+            <br/>
+                <div class="rating-row">⭐⭐⭐⭐ <span className="bold">3.5/5</span></div>
+            </div>
+        </div>
+
     );
 }
 
