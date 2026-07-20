0d7e4ee0e52653badf0e9afe7e558d57975749ea
sharjeelzubairy56-cmyk <sharjeelzubairy56@gmail.com>
2026-07-21 02:33:32 +0500

    fixed some more issues

--- figma-website/src/HappyCustomers.jsx
+++ figma-website/src/HappyCustomers.jsx
@@ -5,7 +5,7 @@ function HappyCustomers() {
           <div className="hc">OUR HAPPY CUSTOMERS</div>
           <div className="h1">
               <div className="r">
-                  <div class="rating-row">⭐⭐⭐⭐</div>
+                  <div className="rating-row">⭐⭐⭐⭐ <span className="bold">3.5/5</span></div>
                   <div className="name">Sarah M.   ✅</div>
                   <div className="reviews1">
                   "I am blown away  by the quality and style of
@@ -13,8 +13,7 @@ function HappyCustomers() {
                       has exceeded my expectations."
                   </div>
                   </div>
-              <div className="r">
-                  <div class="rating-row">⭐⭐⭐⭐</div>
+              <div className="r"><div className="rating-row">⭐⭐⭐⭐ <span className="bold">3.5/5</span></div><div className="rating-row">⭐⭐⭐⭐ <span className="bold">3.5/5</span></div>
                   <div className="name">Alex K.   ✅</div>
                   <div className="reviews1">
                       "I am blown away  by the quality and style of
@@ -23,7 +22,7 @@ function HappyCustomers() {
                   </div>
               </div>
               <div className="r">
-                  <div class="rating-row">⭐⭐⭐⭐</div>
+                  <div className="rating-row">⭐⭐⭐⭐ <span className="bold">3.5/5</span></div>
                   <div className="name">James L.   ✅</div>
                   <div className="reviews1">
                       "I am blown away  by the quality and style of
